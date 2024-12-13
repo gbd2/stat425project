1b96a056279008d1382cf28cebb6f2a741019705
Abbas Shaikh <abts5280@gmail.com>
2024-12-12 23:47:20 -0600

    hierarchical glm

--- code/manufacturer_regression.R
+++ code/manufacturer_regression.R
@@ -6,19 +6,21 @@ regression.data <- read.csv("data/manufacturer_regression_data.csv", header = TR
 
 N <- nrow(regression.data)
 M <- 3
+
 y <- regression.data$top5_count
 groups <- as.integer(factor(regression.data$manufacturer))
-X <- as.matrix(regression.data[,1:5])
+
+X <- as.matrix(regression.data[,1:12])
 
 min_max_norm <- function(x) {
   (x - min(x)) / (max(x) - min(x))
 }
 
-X <- cbind(1, apply(X, 2, min_max_norm))
-
+# X <- cbind(1, apply(X, 2, min_max_norm))
+X <- cbind(1, X)
 
 mu <- rep(0, ncol(X))
-tau <- diag(0.001, ncol(X))
+tau <- diag(0.0001, ncol(X))
 
 jagsData <- list(
   y = y,
@@ -33,7 +35,9 @@ jagsData <- list(
 )
 
 model <- jags.model(file = "code/binomial_glmm.txt", data = jagsData, n.chains=1)
-result <- coda.samples(model, variable.names = c("theta", "gamma"), n.iter=1e4, n.burnin=1e3, n.thin=10)
+result <- coda.samples(model, variable.names = c("theta", "gamma"), n.iter=1e4, n.burnin=1e3)
+
+
 summary(result)
 
 for (i in 1:ncol(X)){
